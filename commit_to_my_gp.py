da858528433b35bd6053212788176e324b63c82d
JiajieZhuo <jiajiezhuo@163.com>
2018-01-26 14:40:26 +0800

    Change the day_of_week to Mon~Sun.

--- commit_to_my_gp.py
+++ commit_to_my_gp.py
@@ -25,5 +25,5 @@ def work_flow():
 
 if __name__ == '__main__':
     scheduler = BlockingScheduler()
-    scheduler.add_job(work_flow, 'cron', day_of_week='1-7', hour=6, minute=0)
+    scheduler.add_job(work_flow, 'cron', day_of_week='0-6', hour=6, minute=0)
     scheduler.start()
